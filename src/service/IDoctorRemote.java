fdd68291cf89edb9e3d24a6289aaeb73fe8953f5
Matth <Matth>
2016-12-07 15:25:29 +0100

    ajout fonction interface remote

--- src/service/IDoctorRemote.java
+++ src/service/IDoctorRemote.java
@@ -1,5 +1,23 @@
 package service;
 
 
-public interface IDoctorRemote {
+
+import entite.*;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public interface IDoctorRemote extends Remote {
+
+    public ArrayList<RDV> consultationRdv() throws RemoteException;
+    public boolean creationOrdonnance(Medecin medecin, Patient patient, String description, String nomCabinet) throws RemoteException;
+    public  ArrayList<Patient> consultationFichePatient() throws  RemoteException;
+    public DossierMedical creationDossierMedical(Patient patient, ArrayList<Ordonnance> listOrdonnance, ArrayList<String> vaccin,
+                                                 String traitementMedical, Medecin medecin) throws RemoteException;
+    public Patient consultationDossierMedical(Patient patient)throws RemoteException;
+    public boolean creationCertificat(Patient patient, Medecin medecin, String motif, Date date, String description, String nomCabinet) throws RemoteException;
+
+
 }
--- src/service/ISecRemote.java
+++ src/service/ISecRemote.java
@@ -8,8 +8,8 @@ import java.util.Date;
 public interface ISecRemote extends Remote{
 
     public RDV creationRdv(Date date, Patient patient, String motif, Medecin medecin) throws RemoteException;
-    public boolean annuleRdv(RDV rdv) throws RemoteException;
-    public boolean modifierRdv(RDV rdv,Date dateheure) throws  RemoteException;
+    public boolean annulationRdv(RDV rdv) throws RemoteException;
+    public boolean modificationRdv(RDV rdv,Date dateheure) throws  RemoteException;
     public Facture creationFacture(Patient patient, Medecin medecin,String nomCabinet,int montant,Date date,String motif) throws  RemoteException;
     public Fourniture creationFourniture(String fournitures, Medecin medecin, Date date, Fournisseur fournisseur, int quantite)throws  RemoteException;
     public FichePatient creationFichePatient(String nom,String prenom, Date inscription, String adresse, int numTel, String mail, int numSec) throws RemoteException;
